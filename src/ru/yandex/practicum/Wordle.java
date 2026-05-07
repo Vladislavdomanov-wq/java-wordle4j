23b1112b22a2aca5b7635e5bd469439cb8c41143
VlaDosick <Mlgtrack@yandex.ru>
2026-05-07 21:24:37 +0300

    fix error test 2

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,12 +1,9 @@
 package ru.yandex.practicum;
-import java.io.File;
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.Scanner;
 
 public class Wordle  {
